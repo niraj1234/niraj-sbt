ad2e6fceaeef9f84771164e5de24284a13c339fe
Niraj <niraj.webtech@gmail.com>
2022-06-11 04:19:52 +0530

    After adding project to GIT hub First Change

--- src/main/java/com/niraj/sbt/NirajSbtApplication.java
+++ src/main/java/com/niraj/sbt/NirajSbtApplication.java
@@ -9,6 +9,7 @@ public class NirajSbtApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NirajSbtApplication.class, args);
 		System.out.println("||| Project Work started NIRAJ-SBT |||==>");
+		System.out.println("Project Added to GIT Hub successfully !");		
 	}
 
 }
