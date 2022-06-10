89e92b83f2569ca8ca5017d6bf09d212e136e5a3
Niraj <niraj.webtech@gmail.com>
2022-06-11 03:40:58 +0530

    NIRAJ-SBT project skeleton to GIT Repo

---
+++ src/main/java/com/niraj/sbt/NirajSbtApplication.java
@@ -0,0 +1,14 @@
+package com.niraj.sbt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NirajSbtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NirajSbtApplication.class, args);
+		System.out.println("||| Project Work started NIRAJ-SBT |||==>");
+	}
+
+}
---
+++ src/test/java/com/niraj/sbt/NirajSbtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niraj.sbt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NirajSbtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
